b7e85b5f5a78c3498cbe935ee1ff335d23455b7e
Ken Faulkner <Ken.Faulkner@gmail.com>
2024-11-24 10:15:54 +1100

    new home for example/tests for jxl-go package

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/kpfaulkner/jxl-go-examples
+
+go 1.22.2
+
+require (
+	github.com/kpfaulkner/jxl-go v0.0.0-20241123230351-60f0f1a4b381
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image/png"
+	"os"
+	"path"
+	"time"
+
+	"github.com/kpfaulkner/jxl-go/core"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Printf("So it begins...\n")
+
+	//defer profile.Start(profile.TraceProfile, profile.ProfilePath(`.`)).Stop()
+	//defer profile.Start(profile.CPUProfile, profile.ProfilePath(`.`)).Stop()
+	//defer profile.Start(profile.BlockProfile, profile.ProfilePath(`.`)).Stop()
+	//defer profile.Start(profile.MemProfileHeap, profile.MemProfileRate(1), profile.ProfilePath(`.`)).Stop()
+	//defer profile.Start(profile.MemProfileAllocs, profile.MemProfileRate(1), profile.ProfilePath(`.`)).Stop()
+
+	file := `../testdata/lossless.jxl`
+	//file := `../testdata/lenna.jxl`
+	//file := `c:\temp\work.jxl`
+	//file := `c:\temp\ken-0-3.jxl`
+	//file := `c:\temp\ken-0-0.jxl`
+
+	// church fails with nested distribution.
+	//file := `../testdata/church.jxl`
+
+	f, err := os.ReadFile(file)
+	if err != nil {
+		log.Errorf("Error opening file: %v\n", err)
+		return
+	}
+
+	r := bytes.NewReader(f)
+	jxl := core.NewJXLDecoder(r, nil)
+	start := time.Now()
+	var jxlImage *core.JXLImage
+	if jxlImage, err = jxl.Decode(); err != nil {
+		fmt.Printf("Error decoding: %v\n", err)
+		return
+	}
+	fmt.Printf("decoding took %d ms\n", time.Since(start).Milliseconds())
+
+	fmt.Printf("Has alpha %v\n", jxlImage.HasAlpha())
+	fmt.Printf("Num extra channels (inc alpha) %d\n", jxlImage.NumExtraChannels())
+
+	// convert to regular Go image.Image
+	img, err := jxlImage.ToImage()
+	if err != nil {
+		fmt.Printf("error when making image %v\n", err)
+	}
+
+	buf := new(bytes.Buffer)
+	if err := png.Encode(buf, img); err != nil {
+		log.Fatalf("boomage %v", err)
+	}
+	ext := path.Ext(file)
+	pngFileName := file[:len(file)-len(ext)] + ".png"
+	err = os.WriteFile(pngFileName, buf.Bytes(), 0666)
+	if err != nil {
+		log.Fatalf("boomage %v", err)
+	}
+}
